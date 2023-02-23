d7253dd59bbb8b416dfefd0bd4881f9fd4b6d758
jtenmo <120563499+jtenmo@users.noreply.github.com>
2023-02-24 00:43:02 +0100

    Update and rename mis_primeros_divs to div/MisDivs.h

---
+++ div/MisDivs.h
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>ejemplo de una division</title>
+</head>
+<body
+<p>Primer parrafo</p>
+<div style="color:rgb(244, 8, 8)">
+<p>Segundo Parrafo</p>
+<p>Tercer Parrafo</p>
+</div>
+<p>Cuarto Parrafo</p>
+</body>
+</html>
